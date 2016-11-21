bae8741c8af1c95ec7008f2e871db77c356dfef6
NabilCherigui <mrtopgamer@hotmail.com>
2016-11-21 21:45:35 +0100

    Star

---
+++ Assets/Scripts/Collector.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collector : MonoBehaviour {
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Star") {
+			Destroy (other.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/StarCounter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarCounter : MonoBehaviour {
+
+	void Update () {
+		if ((GameObject.Find("Star") == null)) {
+			gameObject.GetComponent<Renderer> ().enabled = true;
+		} 
+		else 
+		{
+			gameObject.GetComponent<Renderer> ().enabled = false;
+
+		}
+	}
+}
