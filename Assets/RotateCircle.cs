7c039e727ddc365f373a6c1b275ea643bf9e3407
ninoosewoudt <nino.osewoudt@gmail.com>
2016-11-21 15:54:07 +0100

    op

---
+++ Assets/RotateCircle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class RotateCircle : MonoBehaviour {
+
+    private float _time;
+
+    void Update () {
+        _time += Time.deltaTime;
+        var x = Mathf.Cos (_time);
+        var y = Mathf.Sin (_time);
+        transform.position = new Vector2 (x, y);
+    }
+}
