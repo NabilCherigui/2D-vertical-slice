619cb4e9e448e13fa4849a65b738c0e5d547439a
Rico Evers <ricoevers@live.nl>
2016-11-21 15:54:11 +0100

    reverse gravity

---
+++ Assets/Scripts/ReverseGravity.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReverseGravity : MonoBehaviour {
+	private Rigidbody2D rigidBody;
+
+	void Start ()
+	{
+		rigidBody = GetComponent<Rigidbody2D> ();
+	}
+
+	public void Reverse()
+	{
+		rigidBody.gravityScale *= -1;
+	}
+}
