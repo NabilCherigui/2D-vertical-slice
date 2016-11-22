d44078ba5b896def2369c4aca0eb232fe69d383a
Rico Evers <ricoevers@live.nl>
2016-11-22 11:02:34 +0100

    Stars UI

--- Assets/Scripts/StarCounter.cs
+++ Assets/Scripts/StarCounter.cs
@@ -3,14 +3,22 @@ using System.Collections;
 
 public class StarCounter : MonoBehaviour {
 
-	void Update () {
-		if ((GameObject.Find("Star") == null)) {
-			gameObject.GetComponent<Renderer> ().enabled = true;
-		} 
-		else 
-		{
-			gameObject.GetComponent<Renderer> ().enabled = false;
+	[SerializeField]
+	private GameObject[] _Stars;
+	private int _count;
+	private int _oldCount;
 
+	void Update ()
+	{
+		_count = GameObject.FindGameObjectsWithTag ("Star").Length;
+		for (int i = 0; i < _Stars.Length; i++)
+		{
+			if(_count < _oldCount)
+			{
+				_Stars [i].SetActive (true);
+				_oldCount = _count;
+			}
 		}
+		_oldCount = _count;
 	}
 }
