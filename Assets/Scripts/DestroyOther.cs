db2fba993c3dbf6972bda6e78eb0d74183de4cbc
Rico Evers <ricoevers@live.nl>
2016-12-05 13:53:35 +0100

    Destroy other script edit

--- Assets/Scripts/DestroyOther.cs
+++ Assets/Scripts/DestroyOther.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class DestroyOther : MonoBehaviour
 {
+    [SerializeField]
+    private string tagOfObject;
+
 	void OnTriggerEnter2D(Collider2D other)
 	{
-        if (other.CompareTag("Star"))
+        if (other.CompareTag(tagOfObject))
         {
             Destroy(other.gameObject);
         }
