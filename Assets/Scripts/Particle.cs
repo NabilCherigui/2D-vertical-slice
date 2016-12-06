f7529fa44c564d9f62dd03ddbd7384d0c71722e2
NabilCherigui <mrtopgamer@hotmail.com>
2016-12-06 11:27:53 +0100

    Particle

--- Assets/Scripts/Particle.cs
+++ Assets/Scripts/Particle.cs
@@ -3,13 +3,41 @@ using System.Collections;
 
 public class Particle : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	[SerializeField]
+	private GameObject[] stars;
+
+	[SerializeField]
+	private Vector2[] starPositions;
+
+	[SerializeField]
+	private GameObject particle;
+
+	private Animator animate;
+
+	[SerializeField]
+	private DestroyOther remover;
+
+	void Start (){
+		stars = GameObject.FindGameObjectsWithTag ("Star");
+		for (int i = 0; i < stars.Length; i++) {
+			starPositions[i] = stars[i].transform.position;
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update (){
+		for (int i = 0; i < stars.Length; i++) {
+			if (stars [i] == null) {
+
+				stars[i] = new GameObject ();
+				particle.SetActive (true);
+				particle.transform.position = starPositions [i];
+				StartCoroutine(startAnimation ());
+			}
+		}
+	}
+
+	IEnumerator startAnimation (){
+		yield return new WaitForSeconds (1);
+		particle.SetActive (false);
 	}
 }
