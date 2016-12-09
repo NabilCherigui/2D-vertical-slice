a74bfc6fa2e785da19cef162258b4c9529167467
Rico Evers <ricoevers@live.nl>
2016-12-09 18:25:02 +0100

    map structure update

---
+++ Assets/Scripts/DestroyOnMouseOver.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnMouseOver : MonoBehaviour {
+
+	void OnMouseOver()
+    {
+        if(Input.GetMouseButton(1) || Input.GetMouseButtonDown(1))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
